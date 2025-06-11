1cc3e87d8fc9237af3c709c4d003c5604d5b6480
g.derito <g.derito@reply.it>
2025-06-11 18:22:04 +0200

    readme

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import find_packages, setup
 
+# Read the contents of your README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='agentic-a2a',
     packages=find_packages(include=[
@@ -10,6 +14,15 @@ setup(
     version='0.1.0',
     description='Python framework for implementing multi-agent systems',
     author='devturtleblog@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DevTurtleBlog/agentic",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
     install_requires=['uvicorn', 'fastapi', 'a2a-sdk', 'fastapi-mcp'],
     extras_require={
         'dev': ['pytest', 'pytest-runner'],
