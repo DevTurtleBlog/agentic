55c1486f927a019abb0521e2c77ff923e97866b4
g.derito <g.derito@reply.it>
2025-06-11 18:24:22 +0200

    version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         'agentic.a2a',
         'agentic.mcp'
     ]),
-    version='0.1.0',
+    version='0.1.1',
     description='Python framework for implementing multi-agent systems',
     author='devturtleblog@gmail.com',
     long_description=long_description,
